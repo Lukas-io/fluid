99fdfc5cc063b5c418b79d92ed8204e569099729
Lukas-io <wisdomiyamu@gmail.com>
2025-07-15 00:40:48 +0100

    Initial commit

---
+++ lib/music_post_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MusicPostScreen extends StatelessWidget {
+  const MusicPostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      extendBody: true,
+      body: Column(children: []),
+    );
+  }
+}
